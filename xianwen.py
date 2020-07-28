9888dea8c98a9cc7f1128eb337472a541f439b12
zhangsfdsaf <xiaoxuenice@qq.com>
2020-07-28 08:55:36 -0700

    fsda

--- xianwen.py
+++ xianwen.py
@@ -3,11 +3,15 @@ send_headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKi
 a=requests.get("http://www.360doc.com/content/17/0303/11/13179508_633585381.shtml",headers=send_headers).content.decode("utf-8")
 dd=re.findall('700\;\"\>(.*?)<.*?28px\;\"\>(.*?)\<',a)
 test=[]
+n=0
+for i in dd:
+    if i[1] == "":
+      dd[n+1]+=i
+    n+=1
 for i in dd:
     if i[1] == '':
         continue
     test.append(i)
-test1=[]
 for i in test:
     tt=[]
     for c in i:
@@ -16,11 +20,12 @@ for i in test:
         tt.append(c)
     ee=""
     for b in range(len(tt)-1,1,-1):
-          ee+=i[b]
-    ee+=i[0]
+          ee+=tt[b]
+    ee+=tt[0]
     ee+="\n"
-    ee+=i[1]
+    ee+=tt[1]
     ee+="\n"
-    print(ee)
-    with open('xianwen.txt','a+') as f:
-          f.write(ee)
+    with open("xianwen.txt",'a+') as f:
+       f.write(ee)
+
+
